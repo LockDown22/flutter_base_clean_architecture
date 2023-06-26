2e42f7eb07d55fed1ba7fd13d645d9d6112ab92d
LockDown <82832481+ThanhOP-Creator@users.noreply.github.com>
2023-06-26 22:19:05 +0700

    task(task/#/update_main): config main

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_clean_architecture/gen/strings.g.dart';
-
+import 'configs/app_config.dart';
+import 'di/di.dart' as di;
 import 'application.dart';
 import 'dart:async';
 
 Future<void> main() async {
-  runZonedGuarded(() {
-    WidgetsFlutterBinding.ensureInitialized();
-    LocaleSettings.useDeviceLocale();
+  WidgetsFlutterBinding.ensureInitialized();
+  LocaleSettings.useDeviceLocale();
+  AppConfig.configDev();
+  await di.configureInjection();
+  runZonedGuarded(() async {
     runApp(TranslationProvider(child: const MyApp()));
   }, (error, stack) {});
 }
