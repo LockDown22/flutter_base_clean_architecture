732e7b4058ba6168360e2db2401b9d7e095e3cad
LockDown <82832481+ThanhOP-Creator@users.noreply.github.com>
2023-06-26 22:09:05 +0700

    update di

--- lib/navigator/app_route.gr.dart
+++ lib/navigator/app_route.gr.dart
@@ -9,8 +9,8 @@
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:auto_route/auto_route.dart' as _i3;
-import 'package:flutter_clean_architecture/features/home/home_page.dart' as _i1;
-import 'package:flutter_clean_architecture/features/testpage/test_page.dart'
+import 'package:flutter_clean_architecture/screen/home/home_page.dart' as _i1;
+import 'package:flutter_clean_architecture/screen/testpage/test_page.dart'
     as _i2;
 
 abstract class $AppRouter extends _i3.RootStackRouter {
